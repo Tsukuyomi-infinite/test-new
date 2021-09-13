04480375b06c01d097d52dde828b0d590dab6c9c
infiniteTsukuyomi <devrath.sap91@gmail.com>
2021-09-13 20:43:59 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-	return ('hello welcome to flask AWS test')
-
-@app.route("/services/service-2")
-def hello():
-	return ("This is a sample response from service 2 (Python app service)")
-
-@app.route("/services/service-2/status")
-def status():
-	return ("ok")
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5002, debug=True)
+print ("Hello World")
